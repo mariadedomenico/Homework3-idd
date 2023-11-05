7f0bef39cf7c2a8ba411cd6957cf8f0d7dbeda3d
Elisa Catena <elisacatena00@gmail.com>
2023-11-05 23:29:58 +0100

    Implementazione metodo per leggere la posting list

--- hw3idd/src/main/java/index/PostingListReader.java
+++ hw3idd/src/main/java/index/PostingListReader.java
@@ -2,20 +2,24 @@ package index;
 
 import java.io.IOException;
 import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Map;
 
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
 import org.apache.lucene.index.LeafReader;
+import org.apache.lucene.index.PostingsEnum;
 import org.apache.lucene.index.Terms;
 import org.apache.lucene.index.TermsEnum;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.BytesRef;
 
+import table.Cella;
+
 public class PostingListReader {
 	
-    public static void readPostingList(Path indexPath) throws IOException {
+    @SuppressWarnings("deprecation")
+	public static void readPostingList(Path indexPath, String termToCheck, Map<Cella, Integer> set2count) throws IOException {
     	
         // Apre l'indice
         Directory directory = FSDirectory.open(indexPath);
@@ -24,20 +28,38 @@ public class PostingListReader {
         // Ottieni un LeafReader per accedere ai termini
         LeafReader leafReader = indexReader.leaves().get(0).reader();
 
-        // Campo per cui desideri ottenere il term vector
-        String campo = "cells";
-
         // Ottieni i termini per il campo specificato
-        Terms terms = leafReader.terms(campo);
+        Terms terms = leafReader.terms("cells");
 
         // Itera sui termini
         if (terms != null) {
             TermsEnum termsEnum = terms.iterator();
-            BytesRef term;
-            while ((term = termsEnum.next()) != null) {
-                String termText = term.utf8ToString();
-                long termFrequency = termsEnum.totalTermFreq();
-                System.out.println("Term: " + termText + ", Frequency: " + termFrequency);
+            BytesRef term = new BytesRef(termToCheck);
+            System.out.println("Term: " + term.toString());
+            System.out.println("termsEnum.seekExact(term): " + termsEnum.seekExact(term));
+            if (termsEnum.seekExact(term)) {
+            	termsEnum = terms.iterator();
+	            while ((term = termsEnum.next()) != null) {
+	            	String termValue = term.utf8ToString();
+	            	System.out.println("termValue: " + termValue);
+	                if (termValue.equals(termToCheck)) {
+		                PostingsEnum postingsEnum = termsEnum.postings(null, PostingsEnum.POSITIONS);
+		                int docID;
+	                    while((docID = postingsEnum.nextDoc()) != PostingsEnum.NO_MORE_DOCS) {
+	                        // docID contiene l'ID del documento associato al termine
+	                    	String column = indexReader.document(docID).getField("column").stringValue();
+	                    	String id = indexReader.document(docID).getField("id").stringValue();
+	                    	Cella cella = new Cella(column, id);
+	                    	if(set2count.containsKey(cella)) {
+	                    		set2count.put(cella, set2count.get(cella)+1);
+	                    	}
+	                    	else {
+	                    		set2count.put(cella, 1);
+	                    	}
+	                        System.out.println("Term: " + termValue + ", DocID: " + docID);
+	                    }
+	                }
+	            }
             }
         }
 
